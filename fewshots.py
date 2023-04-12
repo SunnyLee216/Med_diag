ce549dd145bdc45859b4bad5fda480e9654404a4
Sunny Lee <757731661@qq.com>
2023-04-12 19:36:39 +0800

    412

---
+++ fewshots.py
@@ -0,0 +1,45 @@
+RANK_FEW_SHOTS = """Here is the information of the patient:
+Age: 19; Sex: M; Initial evidence: Do you have a fever (either felt or measured with a thermometer)? Yes; Evidence: ; Have you been in contact with a person with similar symptoms in the past 2 weeks? Yes; Do you live with 4 or more people? Yes; Do you attend or work in a daycare? Yes; Do you have pain somewhere, related to your reason for consulting? Yes; Characterize your pain: sensitive; Characterize your pain: burning; Do you feel pain somewhere? tonsil(R); Do you feel pain somewhere? tonsil(L); Do you feel pain somewhere? thyroid cartilage; Do you feel pain somewhere? palace; Do you feel pain somewhere? under the jaw; How intense is the pain? 4; Does the pain radiate to another location? nowhere; How precisely is the pain located? 10; How fast did the pain appear? 4; Do you have a fever (either felt or measured with a thermometer)? Yes; Do you have nasal congestion or a clear runny nose? Yes; Do you have a cough? Yes; Have you traveled out of the country in the last 4 weeks? N
+The list of diseases is as follows:
+['acute laryngitis', 'viral pharyngitis', 'bronchitis', 'urti', 'tuberculosis', 'stable angina', 'unstable angina', 'epiglottitis', 'influenza', 'chagas', 'possible nstemi / stemi']
+RANK: 1. viral pharyngitis; 2. urti; 3. bronchitis; 4. acute laryngitis; 5. tuberculosis; 6. possible nstemi / stemi; 7. influenza; 8. epiglottitis; 9. unstable angina; 10. chagas; 11. stable angina
+
+Here is the information of the patient:
+Age: 26; Sex: M; Initial evidence: Do you constantly feel fatigued or do you have non-restful sleep? Yes; Evidence: ; Do you have a poor diet? Yes; Do you have any family members who have been diagnosed with anemia? Yes; Do you have pain somewhere, related to your reason for consulting? Yes; Characterize your pain: tugging; Characterize your pain: a cramp; Characterize your pain: exhausting; Do you feel pain somewhere? forehead; How intense is the pain? 5; Does the pain radiate to another location? nowhere; How precisely is the pain located? 2; How fast did the pain appear? 0; Are you experiencing shortness of breath or difficulty breathing in a significant way? Yes; Do you feel lightheaded and dizzy or do you feel like you are about to faint? Yes; Do you constantly feel fatigued or do you have non-restful sleep? Yes; Do you have chronic kidney failure? Yes; Have you recently had stools that were black (like coal)? Yes; Are you taking any new oral anticoagulants ((NOACs)? Yes; Is your skin much paler than usual? Yes; Have you traveled out of the country in the last 4 weeks? N; Is your BMI less than 18.5, or are you underweight? Yes
+The list of diseases is as follows:
+['myocarditis', 'anaphylaxis', 'anemia', 'guillain-barré syndrome', 'cluster headache', 'acute pulmonary edema', 'sle', 'chagas', 'myasthenia gravis', 'acute dystonic reactions', ]
+RANK: 1. anemia; 2. anaphylaxis; 3. myocarditis; 4. acute pulmonary edema; 5. panic attack; 6. guillain-barré syndrome; 7. chagas; 8. sle; 9. acute dystonic reactions; 10. myasthenia gravis; 11. cluster headache
+
+Here is the information of the patient:
+Age: 64; Sex: M; Initial evidence: Do you have a fever (either felt or measured with a thermometer)? Yes; Evidence: ; Are you currently being treated or have you recently been treated with an oral antibiotic for an ear infection? Yes; Do you have pain somewhere, related to your reason for consulting? Yes; Characterize your pain: sensitive; Characterize your pain: sharp; Do you feel pain somewhere? ear(R); Do you feel pain somewhere? ear(L); How intense is the pain? 6; Does the pain radiate to another location? nowhere; How precisely is the pain located? 10; How fast did the pain appear? 1; Do you have a fever (either felt or measured with a thermometer)? Yes; Are you more irritable or has your mood been very unstable recently? Yes; Do you have nasal congestion or a clear runny nose? Yes; Have you traveled out of the country in the last 4 weeks? N
+The list of diseases is as follows:
+['chagas', 'urti', 'acute otitis media']
+RANK: 1. acute otitis media; 2. urti; 3. chagas
+"""
+
+RETHINK_FEW_SHOTS = """Here is the information of the patient:
+Age: 19; Sex: M; Initial evidence: Do you have a fever (either felt or measured with a thermometer)? Yes; Evidence: ; Have you been in contact with a person with similar symptoms in the past 2 weeks? Yes; Do you live with 4 or more people? Yes; Do you attend or work in a daycare? Yes; Do you have pain somewhere, related to your reason for consulting? Yes; Characterize your pain: sensitive; Characterize your pain: burning; Do you feel pain somewhere? tonsil(R); Do you feel pain somewhere? tonsil(L); Do you feel pain somewhere? thyroid cartilage; Do you feel pain somewhere? palace; Do you feel pain somewhere? under the jaw; How intense is the pain? 4; Does the pain radiate to another location? nowhere; How precisely is the pain located? 10; How fast did the pain appear? 4; Do you have a fever (either felt or measured with a thermometer)? Yes; Do you have nasal congestion or a clear runny nose? Yes; Do you have a cough? Yes; Have you traveled out of the country in the last 4 weeks? N
+Imagine you are an senior doctor. Based on the previous dialogue, what is the diagnosis? Based on the above conversation, rank the following diseases according to their likelihood of diagnosis. For example, patients are more likely to be diagnosed with disease A than disease B, and disease A should be ranked first in disease B, and so on. 
+The list of diseases is as follows:
+Here is the previous ranking:
+['viral pharyngitis', 'urti', 'bronchitis', 'acute laryngitis', 'tuberculosis', 'possible nstemi / stemi', 'influenza', 'epiglottitis', 'unstable angina', 'chagas', 'stable angina']
+Now,Here is the knwoledge serach from outside:
+
+Regardless of whether external information exists, you should reflect on your previous choices. You need to reconsider whether the diagnosis of this disease is valid.You can change your answer at any point.\nYou should ANSWER:YES/NO and your EXPLANATION for the ANSWER you output.
+"""
+
+
+
+REFLECTIONS = """
+Previous Trial:
+.....
+.....
+
+Reflections:
+
+
+Previous Trial:
+....
+....
+Reflections:
+"""
---
+++ prompts.py
@@ -0,0 +1,109 @@
+from langchain.prompts import PromptTemplate
+
+# DDX
+RANK_PROMPT ="""Imagine you are an senior doctor. Based on the previous dialogue, what is the diagnosis? Based on the above conversation, rank the following diseases according to their likelihood of diagnosis. For example, patients are more likely to be diagnosed with disease A than disease B, and disease A should be ranked first in disease B, and so on. 
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+
+Here is the information of a new patient:
+{information}
+The list of diseases is as follows:
+{diag_list}
+RANK:
+"""
+
+
+
+RETHINK_PROMPT ="""
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+
+Here is the information of a new patient:
+{information}
+The previous ranking of diseases is as follows:
+{previous_rank}
+This is the knowledge of the top 1 diseases:
+{top1_knowledge}
+Assuming that the top 1 disease is correct, Based on the above conversation, what is the diagnosis? 
+Regardless of whether external information exists, you should reflect on your previous choices. You need to reconsider whether the diagnosis of this disease is valid.You can change your RANK at any point.\nYou should RANK again and your EXPLANATION for the RANK you output.
+YES/NO:
+Rationale:
+"""
+
+
+
+SCORE ="""你要给以下的医学推理过程进行打分，分数越高，越有可能是正确的答案。
+评分的细则是：
+- 
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+"""
+
+
+
+
+# 输入各种prompt
+REACT_INSTRUCTION = """Solve a question answering task with interleaving Thought, Action, Observation steps. Thought can reason about the current situation, and Action can be three types: 
+(1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
+(3) Finish[answer], which returns the answer and finishes the task.
+You may take as many steps as necessary.
+Here are some examples:
+{examples}
+Question: {question}{scratchpad}"""
+
+
+REACT_REFLECT_INSTRUCTION = """Solve a question answering task with interleaving Thought, Action, Observation steps. Thought can reason about the current situation, and Action can be three types: 
+(1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
+(3) Finish[answer], which returns the answer and finishes the task.
+You may take as many steps as necessary.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+
+{reflections}
+
+Question: {question}{scratchpad}"""
+
+REFLECTION_HEADER = 'You have attempted to answer following question before and failed. The following reflection(s) give a plan to avoid failing to answer the question in the same way you did previously. Use them to improve your strategy of correctly answering the given question.\n'
+
+REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given access to an Docstore API environment and a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>], or you used up your set number of reasoning steps. In a few sentences, Diagnose a possible reason for failure and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.  
+Here are some examples:
+{examples}
+
+Previous trial:
+Question: {question}{scratchpad}
+
+Reflection:"""
+
+rank_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "information", "diag_list"],
+                        template = RANK_PROMPT,
+                        )
+rethink_agnet_prompt = PromptTemplate(
+                        input_variables=["examples","previsious_rank", "top1_knowledge", "information"],
+                        template = RETHINK_PROMPT,
+                        )
+score_agent_prompt = PromptTemplate(
+                        input_variables=["examples"],
+                        template=SCORE,
+                        )
+
+react_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "scratchpad"],
+                        template = REACT_INSTRUCTION,
+                        )
+
+react_reflect_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "reflections", "question", "scratchpad"],
+                        template = REACT_REFLECT_INSTRUCTION,
+                        )
+
+reflect_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "scratchpad"],
+                        template = REFLECT_INSTRUCTION,
+                        )
